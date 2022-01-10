f2e3c34e231591e0e6e0c6d3c83bb181718a9752
Tobias Pieber <tobias.pieber@htl-braunau.at>
2022-01-10 11:52:26 +0100

    Added IApi interface

---
+++ Strava.NET/API/IApi.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StravaSharp.API
+{
+    internal interface IApi
+    {
+        string BaseUrl { get; set; }
+
+        Task<object> HttpGetAsync(string url_parameters, StringContent string_content);
+        Task<object> HttpPostAsync(string url_parameters, StringContent string_content);
+        Task<object> HttpPutAsync(string url_parameters, StringContent string_content);
+        Task<object> HttpDeleteAsync(string url_parameters, StringContent string_content);
+    }
+}
